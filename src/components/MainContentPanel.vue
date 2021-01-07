c02fbaa782071e843fd4a2d4fc8eda0d765f8c05
Kevan <kevinjbritten@gmail.com>
2021-01-07 14:49:07 -0500

    added view display components

---
+++ src/components/MainContentPanel.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="content-panel" :class="{ 'content-panel--bg': isBg }">
+    <div class="content-panel__triangle content-panel__triangle--top"></div>
+    <div
+      class="content-panel__content"
+      :class="{ 'content-panel__content--bg': isBg }"
+    >
+      <slot></slot>
+    </div>
+    <div class="content-panel__triangle content-panel__triangle--bottom"></div>
+  </div>
+</template>
+
+<script>
+export default { props: ["isBg"] };
+</script>
+
+<style scoped>
+.content-panel {
+  /* refactor with sass */
+  width: 90%;
+  position: absolute;
+  left: 10%;
+  z-index: 2;
+  padding-top: 10%;
+  &--bg {
+    z-index: 1;
+    padding-top: 0px;
+    left: 0;
+  }
+
+  &__content {
+    position: relative;
+    background-color: rgb(var(--main-bg-color), 0.8);
+    height: 380px;
+    z-index: 10;
+  }
+
+  &__triangle {
+    position: relative;
+    width: 100%;
+    min-height: 20px;
+    height: 40px;
+    background-color: rgba(var(--main-bg-color), 0.8);
+
+    &--top {
+      clip-path: polygon(
+        100% calc(var(--triangle-angle) * 1%),
+        0 100%,
+        100% 100%
+      );
+    }
+
+    &--bottom {
+      clip-path: polygon(
+        100% 0%,
+        0 calc((100 - var(--triangle-angle)) * 1%),
+        0% 0%
+      );
+    }
+  }
+}
+</style>
---
+++ src/components/ViewDisplay.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="view-display">
+    <slot></slot>
+    <transition name="slide">
+      <main-content-panel :isBg="false" v-if="displayIsOpened">
+        <slot></slot>
+      </main-content-panel>
+    </transition>
+    <transition name="slide">
+      <main-content-panel :isBg="true" v-if="displayIsOpened">
+        <div class="content-panel__content--background"></div>
+      </main-content-panel>
+    </transition>
+  </div>
+</template>
+
+<script>
+import MainContentPanel from "./MainContentPanel.vue";
+export default {
+  props: ["displayIsOpened"],
+  components: {
+    MainContentPanel
+  }
+};
+</script>
+
+<style scoped>
+.slide-enter {
+  transform: translateX(-100%);
+}
+.slide-enter-active,
+.slide-leave-active {
+  transition: transform 0.3s ease-out;
+}
+.slide-enter-to {
+  transform: translateX(0%);
+}
+
+.slide-leave-to {
+  transform: translateX(-100%);
+}
+
+.content-panel__background {
+  width: 110%;
+  z-index: -1;
+}
+/* .content-panel__content--background {
+  height: 100px;
+} */
+</style>
