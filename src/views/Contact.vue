567327c94502f72ae324fe410cdddc722e9f19e9
Kevan <kevinjbritten@gmail.com>
2021-02-06 15:31:58 -0500

    added placeholder merch and contact

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,9 +1,26 @@
 <template>
-  <div><h1>Contact</h1></div>
+  <div>
+    <h1>Contact</h1>
+    <p>
+      Email address for booking inquiries or any other information, contact
+      Naghmehasong@gmail.com
+    </p>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style scoped>
+h1 {
+  color: black;
+  width: 100%;
+  text-align: center;
+}
+p {
+  margin-top: 50px;
+  color: black;
+  text-align: center;
+}
+</style>
--- src/views/Merch.vue
+++ src/views/Merch.vue
@@ -1,9 +1,62 @@
 <template>
-  <div><h1>Merch</h1></div>
+  <div class="merch-page">
+    <scroll-area :title="'Merch'">
+      <div class="item-container">
+        <h1>CD</h1>
+        <img src="../assets/images/20191212_103032.jpg" />
+        <p>
+          $10 on
+          <a
+            href="https://naghmehandthesouthernshores.bandcamp.com/album/naghmeh-and-the-southern-shores"
+            target="_blank"
+            >bandcamp</a
+          >
+        </p>
+      </div>
+      <div class="item-container">
+        <h1>T-shirt</h1>
+        <img src="../assets/images/20191212_110100.jpg" />
+        <p>
+          $15 on
+          <a
+            href="https://naghmehandthesouthernshores.bandcamp.com/merch/tank-top"
+            target="_blank"
+            >bandcamp</a
+          >
+        </p>
+      </div>
+    </scroll-area>
+  </div>
 </template>
 
 <script>
-export default {};
+import ScrollArea from "../components/ScrollArea.vue";
+
+export default {
+  components: { ScrollArea }
+};
 </script>
 
-<style></style>
+<style scoped>
+a {
+  color: black;
+}
+p {
+  color: black;
+}
+h1 {
+  color: black;
+}
+.merch-page {
+  height: 100%;
+  width: 100%;
+}
+.item-container {
+  margin: 10px 0 30px;
+  text-align: center;
+}
+img {
+  height: 150px;
+  width: 100%;
+}
+</style>
